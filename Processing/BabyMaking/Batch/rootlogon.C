825064d0060abf4244244b249e833b4857c27964
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2014-12-04 12:05:56 -0800

    First implementation of fat-jetting on-the-fly

---
+++ Processing/BabyMaking/Batch/rootlogon.C
@@ -0,0 +1,6 @@
+{
+     gSystem->Load("libfastjet.so");
+     //gSystem->Load("../../../../../../../fastjet-install/lib/libfastjet.so");
+     //gSystem->AddIncludePath("/homes/jaehyeok/Analysis/MJ/fastjet-install/include/");
+
+}
