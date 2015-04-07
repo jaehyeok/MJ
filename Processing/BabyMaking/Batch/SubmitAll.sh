8e833f7297c301bfbd14d606eaa204ca67ba6e27
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2015-04-07 00:40:27 -0700

    code used to make Phys14_04Apr2015 babies

--- Processing/BabyMaking/Batch/SubmitAll.sh
+++ Processing/BabyMaking/Batch/SubmitAll.sh
@@ -9,8 +9,8 @@ do
 
     DATASET=`echo $line | awk '{print $1}'`
     echo "Done with submitting $DATASET"
-    echo "Now wait 45 minutes before submitting jobs for next dataset "
-    sleep 1800 # wait for 90 min before submitting jobs for next dataset 
+    echo "Now wait 90 minutes before submitting jobs for next dataset "
+    sleep 5400 # wait for 90 min before submitting jobs for next dataset 
     echo -e "\033[5;34m-----------------------------------------------------------------\033[0m"
     echo -e "\033[5;34m-----------------------------------------------------------------\033[0m"
 
