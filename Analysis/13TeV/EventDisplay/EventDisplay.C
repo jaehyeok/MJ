7bb561ce7c30d2907075f1d152118de0d7a650f0
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2016-03-19 14:58:20 +0900

    Added EventDisplay.C that works with M&A's babies

--- Analysis/13TeV/EventDisplay/EventDisplay.C
+++ Analysis/13TeV/EventDisplay/EventDisplay.C
@@ -23,9 +23,6 @@
 #include "../babytree_manuel.h"
 
 using namespace std;
-int pTR0p5          =0;
-int FatjetpTthres   =0;
-int mjthres         =0;
 
 void myText(Double_t x,Double_t y, const char *text, Color_t red,float tsize) {
 
@@ -65,9 +62,7 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
     // chain      
     TChain *ch        = new TChain("tree");
     if(!sig) ch->Add("babies/small_quick_cfA_746p1_nleps1_trig0ON.root");
-    //if(sig) ch->Add("babies/small_quick_cfA_751_nleps1_trig0ON.root");
-    //if(sig) ch->Add("small_quick_HTMHT_Run2015C-PromptReco-v1_MINIAOD_v83.root");
-    if(sig) ch->Add("2015_11_05/data/singlelep/combined/skim_1lht500met200/*root");
+    if(sig) ch->Add("~/scratch/plots/1d_2015d_13Jan2016/2d/48ipb/*.root");
     
     InitBaby(ch); 
    
@@ -76,28 +71,12 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
     {
         ch->GetEntry(i); 
 
-        if(event>0 && event_!=event) continue; // FIXME   
+        // apply selections (event==-1 && mj_>xxx && ht_>yyy && ...)
+        // or 
+        // select an event (event!=-1 && event_=xxxxxx)
+        // "event" is one of the arguments  
         
-        // Nfatjet, MJ, mj sorting 
-        int Nfatjet_thres = 0;
-        double MJ_thres=0; 
-        vector<double> mj_thres_sorted; 
-        vector<int> mj_thres_sorted_index; 
-        for(int ifj=0; ifj<(int)FatjetPt_->size(); ifj++)
-        {   
-            if(FatjetPt_->at(ifj)<FatjetpTthres) continue; 
-            if(mj_->at(ifj)<mjthres) continue; 
-            MJ_thres = MJ_thres + mj_->at(ifj);   
-            Nfatjet_thres++;
-        } 
-
-        double mT;
-        //if(RA4MusPt_->size()==1) 
-        //    mT  = TMath::Sqrt( 2*MET_*RA4MusPt_->at(0)*(1-TMath::Cos(METPhi_-RA4MusPhi_->at(0))) ); 
-        //if(RA4ElsPt_->size()==1) 
-        //    mT  = TMath::Sqrt( 2*MET_*RA4ElsPt_->at(0)*(1-TMath::Cos(METPhi_-RA4ElsPhi_->at(0))) ); 
-        mT  = mt_; 
-
+        if(event>0 && event_!=event) continue; 
 
         // 
         //  Event Display
@@ -112,17 +91,17 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
         else npert=3;
         float xalign = 0.82;
         vector<TMarker> constituents, genpart;
-        TEllipse *cone[FatjetPt_->size()]; 
+        TEllipse *cone[fjets_pt_->size()]; 
         TH2F *h_fatjets = new TH2F("h_fatjets","h_fatjets", 230, -5.0, 5.0, 144, -3.141592, 3.141592); 
         cout << event_ << endl;
-        for(int ifj = 0; ifj< (int)FatjetPt_->size(); ifj++)
+        for(int ifj = 0; ifj< (int)fjets_pt_->size(); ifj++)
         {   if(ifj==0) cout << "... Fatjets info(pT, eta, phi, mj)" << endl;
-            h_fatjets->Fill(FatjetEta_->at(ifj),FatjetPhi_->at(ifj), mj_->at(ifj));
-            cout << FatjetPt_->at(ifj) << " " << FatjetEta_->at(ifj) << " " 
-                 <<  FatjetPhi_->at(ifj) << " " << mj_->at(ifj) << endl;
+            h_fatjets->Fill(fjets_eta_->at(ifj),fjets_phi_->at(ifj), fjets_m_->at(ifj));
+            cout << fjets_pt_->at(ifj) << " " << fjets_eta_->at(ifj) << " " 
+                 <<  fjets_phi_->at(ifj) << " " << fjets_m_->at(ifj) << endl;
         }
 
-        TString cname = Form("Event_%i_",event_);
+        TString cname = Form("Event_%lli_",event_);
         TCanvas *c = new TCanvas(cname,cname,1640,760);
         gPad->SetRightMargin(0.35);
         h_fatjets->SetZTitle("m_{j} [GeV]");
@@ -135,13 +114,13 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
         gPad->Update();
 
         TPaletteAxis *palette = (TPaletteAxis*)h_fatjets->GetListOfFunctions()->FindObject("palette");
-        for(int ifj = 0; ifj< (int)FatjetPt_->size(); ifj++)
+        for(int ifj = 0; ifj< (int)fjets_pt_->size(); ifj++)
         {
-            cone[ifj] = new TEllipse(FatjetEta_->at(ifj),FatjetPhi_->at(ifj), 1.2, 1.2);
+            cone[ifj] = new TEllipse(fjets_eta_->at(ifj),fjets_phi_->at(ifj), 1.2, 1.2);
             //  cone[ifj]->SetFillStyle(3003);
             cone[ifj]->SetFillStyle(0);
             Int_t binx,biny,binz;
-            h_fatjets->GetBinXYZ(h_fatjets->FindBin(FatjetEta_->at(ifj),FatjetPhi_->at(ifj)),binx,biny,binz);
+            h_fatjets->GetBinXYZ(h_fatjets->FindBin(fjets_eta_->at(ifj),fjets_phi_->at(ifj)),binx,biny,binz);
             //cout<<"bin x bin y "<<binx<<" "<<biny<<endl;
             //cout<<"content "<<h_fatjets->GetBinContent(binx,biny)<<endl;
 
@@ -157,36 +136,36 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
         float lepPt, lepEta, lepPhi; 
         if(nels_==1)  
         {
-            for(int i=0; i<RA4ElsPt_->size(); i++) 
+            for(int i=0; i<(int)els_pt_->size(); i++) 
             {
-                if(RA4ElsMinIso_->at(i)>0.1) continue;
-                if(IsRA4Els_->at(i)!=1) continue;
-                if(RA4ElsPt_->at(i)<20) continue;
-                lepPt   =RA4ElsPt_->at(i);
-                lepEta  =RA4ElsEta_->at(i);
-                lepPhi  =RA4ElsPhi_->at(i); 
+                if(els_miniso_->at(i)>0.1) continue;
+                if(els_sigid_->at(i)!=1) continue;
+                if(els_pt_->at(i)<20) continue; 
+                lepPt   =els_pt_->at(i);
+                lepEta  =els_eta_->at(i);
+                lepPhi  =els_phi_->at(i); 
             }
         }
         if(nmus_==1)  
         {
-            for(int i=0; i<RA4MusPt_->size(); i++) 
+            for(int i=0; i<(int)mus_pt_->size(); i++) 
             {
-                if(RA4MusMinIso_->at(i)>0.2) continue;
-                if(IsRA4Mus_->at(i)!=1) continue;
-                if(RA4MusPt_->at(i)<20) continue;
-                lepPt   =RA4MusPt_->at(i);
-                lepEta  =RA4MusEta_->at(i);
-                lepPhi  =RA4MusPhi_->at(i); 
+                if(mus_miniso_->at(i)>0.2) continue;
+                if(mus_sigid_->at(i)!=1) continue;
+                if(mus_pt_->at(i)<20) continue;
+                lepPt   =mus_pt_->at(i);
+                lepEta  =mus_eta_->at(i);
+                lepPhi  =mus_phi_->at(i); 
             }
         }
 
 
 
-        myText(xalign+0.01,0.96,Form("H_{T} = %.0f GeV",HT_),1,0.04);
-        myText(xalign+0.01,0.91,Form("M_{J} = %.0f GeV",MJ_thres),1,0.04);
-        myText(xalign+0.01,0.86,Form("#slash{E}_{T} = %.0f GeV",MET_),1,0.04);
-        myText(xalign+0.01,0.81,Form("m_{T} = %.0f GeV",mT),1,0.04);
-        myText(xalign+0.01,0.76,Form("reco %s p_{T} = %.0f GeV", nvmus_==1?"#mu":"e", lepPt), kBlack, 0.04); 
+        myText(xalign+0.01,0.96,Form("H_{T} = %.0f GeV",ht_),1,0.04);
+        myText(xalign+0.01,0.91,Form("M_{J} = %.0f GeV",mj_),1,0.04);
+        myText(xalign+0.01,0.86,Form("#slash{E}_{T} = %.0f GeV",met_),1,0.04);
+        myText(xalign+0.01,0.81,Form("m_{T} = %.0f GeV",mt_),1,0.04);
+        myText(xalign+0.01,0.76,Form("reco %s p_{T} = %.0f GeV", nmus_==1?"#mu":"e", lepPt), kBlack, 0.04); 
         nextline=0.76;
         TMarker recolep = TMarker(lepEta, lepPhi, 27);
         recolep.SetMarkerSize(4);
@@ -202,10 +181,10 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
         myText(xalign+0.01,0.71,Form("-- large-R jets --"),1,0.04); 
         myText(xalign+0.01,0.67,Form("(pT, eta, phi, mass)"),1,0.03); 
         nextline=0.635;
-        for(int ifj = 0; ifj< (int)FatjetPt_->size(); ifj++)
+        for(int ifj = 0; ifj< (int)fjets_pt_->size(); ifj++)
         {
             myText(xalign+0.01,nextline,Form("%3.0f, %2.1f, %2.1f, %3.0f",
-                   FatjetPt_->at(ifj),FatjetEta_->at(ifj),FatjetPhi_->at(ifj),mj_->at(ifj)),1,0.03); 
+                   fjets_pt_->at(ifj),fjets_eta_->at(ifj),fjets_phi_->at(ifj),fjets_m_->at(ifj)),1,0.03); 
             nextline=nextline-0.035;
         } 
         nextline=nextline-0.015;
@@ -213,11 +192,11 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
         nextline=nextline-0.04;
         myText(xalign+0.01,nextline,Form("(pT, eta, phi)"),1,0.03); 
         nextline=nextline-0.035;
-        for(int ij = 0; ij< (int)JetPt_->size(); ij++)
+        for(int ij = 0; ij< (int)jets_pt_->size(); ij++)
         {            
-            if(JetIsLep_->at(ij)==1) continue;
+            if(jets_islep_->at(ij)==1) continue;
             myText(xalign+0.01,nextline,Form("%3.0f, %2.1f, %2.1f",
-                   JetPt_->at(ij),JetEta_->at(ij),JetPhi_->at(ij)),1,0.03); 
+                   jets_pt_->at(ij),jets_eta_->at(ij),jets_phi_->at(ij)),1,0.03); 
             nextline=nextline-0.035;
         }
         
@@ -232,7 +211,7 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
 
         TArrow line1 = TArrow();
         if(truth)line1.DrawLineNDC(xalign-0.02,start+0.035,xalign+0.14,start+0.035);
-        TMarker met = TMarker(0, METPhi_, 27);
+        TMarker met = TMarker(0, met_phi_, 27);
         met.SetMarkerSize(4);
         met.SetMarkerColor(kMagenta);
         genpart.push_back(met);
@@ -265,14 +244,14 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
         //else nextline = start - 2*npert*increment-offset;
 
 
-        for(int ij=0;ij<(int)JetPt_->size();ij++)
+        for(int ij=0;ij<(int)jets_pt_->size();ij++)
         {
-            if(JetIsLep_->at(ij)==1) continue;
+            if(jets_islep_->at(ij)==1) continue;
             if(ij==0) cout << "... skinny jet info (pT eta phi)" << endl;
-            cout << JetPt_->at(ij) << " " << JetEta_->at(ij) << " " <<  JetPhi_->at(ij) << endl;
-                TMarker jet = TMarker(JetEta_->at(ij),JetPhi_->at(ij),20);
+            cout << jets_pt_->at(ij) << " " << jets_eta_->at(ij) << " " <<  jets_phi_->at(ij) << endl;
+                TMarker jet = TMarker(jets_eta_->at(ij),jets_phi_->at(ij),20);
                 jet.SetMarkerSize(1.2);
-                if(JetCSV_->at(ij)>0.890) jet.SetMarkerColor(8);
+                if(jets_csv_->at(ij)>0.890) jet.SetMarkerColor(8);
                 constituents.push_back(jet);
         }
 
@@ -313,11 +292,11 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
             TLorentzVector vt[4];
             TLorentzVector vLSP[2];
 
-            for(unsigned int imc = 0; imc < GenId_->size(); imc++)
+            for(unsigned int imc = 0; imc < mc_id_->size(); imc++)
             {
-                int id= (int)GenId_->at(imc);
+                int id= (int)mc_id_->at(imc);
                 int absid = abs(id);
-                int mid = (int)GenMId_->at(imc);
+                int mid = (int)mc_mom_->at(imc);
                 int absmid= abs(mid);
                 if(id!=mid)
                 {
@@ -326,7 +305,7 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
                     int color=0;
                     bool flag=false;
                     if(id==1000021){
-                        if(gl<2) vgl[gl].SetPtEtaPhiM(GenPt_->at(imc), GenEta_->at(imc), GenPhi_->at(imc), 1500);
+                        if(gl<2) vgl[gl].SetPtEtaPhiM(mc_pt_->at(imc), mc_eta_->at(imc), mc_phi_->at(imc), 1500);
                         gl++;
                     }  
 
@@ -351,7 +330,7 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
                         color = col[top];
                         if(!sig) {}
                         if(sig) {if(top>1){ nextline-=(increment+offset);}}
-                        if(top<4)vt[top].SetPtEtaPhiM(GenPt_->at(imc), GenEta_->at(imc), GenPhi_->at(imc), 172.5); 
+                        if(top<4)vt[top].SetPtEtaPhiM(mc_pt_->at(imc), mc_eta_->at(imc), mc_phi_->at(imc), 172.5); 
                         top++;
                     }
 
@@ -426,14 +405,14 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
 
                     if(flag)
                     {
-                        TMarker temp = TMarker(GenEta_->at(imc),GenPhi_->at(imc), marknum);
+                        TMarker temp = TMarker(mc_eta_->at(imc),mc_phi_->at(imc), marknum);
                         TMarker legmark = TMarker(0.81,nextline,marknum);
                         legmark.SetNDC();
                         legmark.SetX(xalign+0.01);
                         legmark.SetY(nextline+0.011);
-                        TString label = partname + "  " + Form("%.0f",GenPt_->at(imc));
+                        TString label = partname + "  " + Form("%.0f",mc_pt_->at(imc));
                         myText(xalign+0.03,nextline,partname,color,0.04);
-                        myText(xalign+0.07,nextline,Form("%.0f",GenPt_->at(imc)),color,0.04);
+                        myText(xalign+0.07,nextline,Form("%.0f",mc_pt_->at(imc)),color,0.04);
                         temp.SetMarkerColor(color);
                         legmark.SetMarkerColor(color);
                         temp.SetMarkerSize(2);
@@ -511,20 +490,20 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
             constituents[iy].Draw("same");
         }
 
-        for(int ifj = 0; ifj< (int)FatjetPt_->size(); ifj++)
+        for(int ifj = 0; ifj< (int)fjets_pt_->size(); ifj++)
         {
             if(fatjet) cone[ifj]->Draw();
         }
         h_fatjets->Draw("colz same");
-        h_fatjets->SetTitle(Form("Event %i",event_));
+        h_fatjets->SetTitle(Form("Event %lli",event_));
 
         h_fatjets->SetStats(0); 
         h_fatjets->SetXTitle("#eta"); 
         h_fatjets->SetYTitle("#phi"); 
 
         TString savename;
-        if(!sig) savename = "fig/EventDisplay_"+cname+Form("%i_FJ",Nfatjet_thres); 
-        else  savename = "fig/EventDisplay_"+cname+Form("%i_FJ",Nfatjet_thres);
+        if(!sig) savename = "fig/EventDisplay_"+cname+Form("%i_FJ",nfjets_); 
+        else  savename = "fig/EventDisplay_"+cname+Form("%i_FJ",nfjets_);
         if(truth) savename+="_truth";
         else savename+="_notruth";	  
         if(fatjet) savename+="_fatjet";
@@ -540,29 +519,6 @@ void EventDisplayForBaby(bool sig=false, bool truth=true, bool fatjet=true, int 
 void EventDisplay(int event=1) 
 {
 /*
-    event = 137500577; 
-    EventDisplayForBaby(false, false, true, event);
-    EventDisplayForBaby(true, false, true, event);
-    event = 182780721; 
-    EventDisplayForBaby(false, false, true, event);
-    EventDisplayForBaby(true, false, true, event);
-    event = 196357352; 
-    EventDisplayForBaby(false, false, true, event);
-    EventDisplayForBaby(true, false, true, event);
-    event = 259602423; 
-    EventDisplayForBaby(false, false, true, event);
-    EventDisplayForBaby(true, false, true, event);
-    event = 286133020; 
-    EventDisplayForBaby(false, false, true, event);
-    EventDisplayForBaby(true, false, true, event);
-*/
-/*
-    event = 77936145; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 118979701; 
-    EventDisplayForBaby(true, false, true, event);
-*/
-/*
     // R4 
     event = 112119467; 
     EventDisplayForBaby(true, false, true, event);
@@ -572,35 +528,7 @@ void EventDisplay(int event=1)
     EventDisplayForBaby(true, false, true, event);
     event = 183543170; 
     EventDisplayForBaby(true, false, true, event);
-
-
-    // R3 high mT evets 
-    event = 408328426; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 1589198284; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 35509686; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 566293952; 
-    EventDisplayForBaby(true, false, true, event);
-
-    // R2 high MJ evets
-    event = 235514804; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 192006307; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 182740588; 
-    EventDisplayForBaby(true, false, true, event);
-    event = 343164899; 
-    EventDisplayForBaby(true, false, true, event);
 */
-
-    // R2 : MET>400 nbm>=2
-    event = 70779576;
+    event = 943292398;
     EventDisplayForBaby(true, false, true, event);
-    event = 338548924;
-    EventDisplayForBaby(true, false, true, event);
-    event = 35509686;
-    EventDisplayForBaby(true, false, true, event);
-
 }
